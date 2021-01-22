c6ac6031341139da6e9639ae21f0549cb5f94cd9
Sidi Becker <sidibecker@hotmail.com>
2021-01-21 21:39:09 -0300

    useRef #2

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,13 +2,32 @@ import React, { useEffect, useRef, useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from '../../components/layout/SectionTitle';
 
-const UseRef = (props) => {
+const UseRef = () => {
+  const merge = function (str1, str2) {
+    const list = Array.from(new Array(Math.max(str1.length, str2.length)));
+
+    return list
+      .map((_, i) => `${[...str1][i] || ''}${[...str2][i] || ''}`)
+      .join('');
+  };
+
   const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+
   const count = useRef(0);
+
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
 
   useEffect(() => {
     count.current++;
+    myInput2.current.focus();
   }, [value1]);
+
+  useEffect(() => {
+    count.current++;
+    myInput1.current.focus();
+  }, [value2]);
 
   return (
     <div className="UseRef">
@@ -17,12 +36,12 @@ const UseRef = (props) => {
         subtitle="Retorna um objeto mutável com a propriedade .current!"
       />
 
-      <SectionTitle title="Exercício 01" />
+      <SectionTitle title="Exercício #01" />
 
       <div className="center">
         <div>
           <span className="text">Valor: </span>
-          <span className="text">{value1} [</span>
+          <span className="text">{merge(value1, value2)} [</span>
           <span className="text red">{count.current}</span>
           <span className="text">]</span>
         </div>
@@ -31,7 +50,19 @@ const UseRef = (props) => {
           type="text"
           className="input"
           value={value1}
+          ref={myInput1}
           onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          value={value2}
+          ref={myInput2}
+          onChange={(e) => setValue2(e.target.value)}
         />
       </div>
     </div>
